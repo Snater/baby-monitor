b5bf1d6e7cc737c3887ed3157fe7d2ccc0ace698
Snater <git@snater.com>
2025-03-09 14:09:51 +0100

    Added time input

--- src/app/actions/addAmount.ts
+++ src/app/actions/addAmount.ts
@@ -4,13 +4,14 @@ import promisePool from '@/lib/mysql';
 
 export default async function addAmount(formData: FormData) {
 	const amount = formData.get('amount');
+	const datetime = formData.get('datetime');
 
-	if (typeof amount !== 'string') {
+	if (typeof amount !== 'string' || typeof datetime !== 'string') {
 		return;
 	}
 
 	promisePool.query(
 		'INSERT INTO events (amount, time) VALUES (?, ?)',
-		[parseInt(amount), new Date()]
+		[parseInt(amount), new Date(datetime)]
 	);
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,30 @@
+'use client'
+
 import Form from 'next/form';
 import addAmount from '@/app/actions/addAmount';
+import {useEffect, useState} from 'react';
+
+function getLocalDate(date?: Date) {
+	const now = date ?? new Date();
+	now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+	return now;
+}
 
 export default function AmountForm() {
+	const [selectedTime, setSelectedTime] = useState<Date>(getLocalDate());
+	const [stopUpdatingTime, setStopUpdatingTime] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (stopUpdatingTime) {
+			return;
+		}
+
+		const intervalId = setInterval(() => {
+			setSelectedTime(getLocalDate());
+		}, 5000);
+
+		return () => clearInterval(intervalId);
+	}, [stopUpdatingTime]);
 
 	return (
 		<Form action={addAmount}>
@@ -9,31 +32,49 @@ export default function AmountForm() {
 				<div className="pb-4">
 					<h2>🍼 Let&#39;s get some milk!</h2>
 
-					<label htmlFor="amount" className="block text-sm/6 font-medium text-gray-900">
-						Custom Amount
-					</label>
-					<div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-						<div className="sm:col-span-3">
-							<div>
-								<div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-									<input
-										id="amount"
-										min={0}
-										name="amount"
-										type="number"
-										className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-									/>
+					<div className="grid gap-3">
+
+						<div>
+							<label className="block" htmlFor="datetime">Time</label>
+							<div className="input-container">
+								<input
+									className="min-w-0 grow w-full"
+									id="datetime"
+									name="datetime"
+									onChange={event => setSelectedTime(getLocalDate(new Date(event.target.value)))}
+									onFocus={() => setStopUpdatingTime(true)}
+									type="datetime-local"
+									value={selectedTime.toISOString().slice(0, 16)}
+								/>
+							</div>
+						</div>
+
+						<div>
+							<label htmlFor="amount">
+								Custom Amount
+							</label>
+							<div className="grid grid-cols-1 gap-x-3 sm:grid-cols-2">
+								<div className="sm:col-span-1">
+									<div className="input-container">
+										<input
+											id="amount"
+											min={0}
+											name="amount"
+											type="number"
+										/>
+									</div>
+								</div>
+								<div className="sm:col-span-1">
+									<button
+										type="submit"
+										className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>
+										Submit
+									</button>
 								</div>
 							</div>
 						</div>
-						<div className="sm:col-span-3">
-							<button
-								type="submit"
-								className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-							>
-								Submit
-							</button>
-						</div>
+
 					</div>
 
 				</div>
